30c117fe8613d16b2bc7c66b9f6d7f0f6cc901cc
Mo Morsi <mmorsi@redhat.com>
2013-02-05 18:35:54 -0500

    tweak bootstrap script

--- bin/omega-bootstrap.sh
+++ bin/omega-bootstrap.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
+echo "Installing bootstrap dependencies and checking out components"
+echo "This may take a few minutes to complete"
+
 yum install git puppet
 git clone http://github.com/movitto/omega-conf
 cd omega-conf
-puppet apply ./recipes/omega/omega.pp --modulepath=./recipes
 
-echo "Skipping over private data installation"
-#puppet apply ./recipes/omega/mediawiki.pp --modulepath=./recipes
-#puppet apply ./recipes/omega/verify.pp    --modulepath=./recipes
+echo "Copy private data into private/ directory and press any key to begin installation"
+read
+
+puppet apply ./recipes/omega/omega.pp     --modulepath=./recipes
+puppet apply ./recipes/omega/mediawiki.pp --modulepath=./recipes
+puppet apply ./recipes/omega/verify.pp    --modulepath=./recipes
